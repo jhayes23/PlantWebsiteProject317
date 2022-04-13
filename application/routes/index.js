e791a9b368cbb88da38f34a483ae5716e0f7b8a4
josh <rainiser@icloud.com>
2022-04-12 21:49:53 -0700

    Added login + Register hbs. still need to do navbar, banner, and other pages.

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Joshua Hayes" });
 });
 
+/* GET login page. */
+router.get('/login', (req,res,next)=>{
+  res.render('login', { title: 'Login'});
+});
+
+/* GET register page. */
+router.get('/register',(req,res,next)=>{
+  res.render('register', { title: 'Register'});
+});
+
+
+router.get('users', (req,res,next)=>{
+  res.render('users');
+})
 module.exports = router;
