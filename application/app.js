693a3039908354e71b8e6773e5738b586af43642
josh <rainiser@icloud.com>
2022-04-25 12:50:01 -0700

    Added sessions, password hashing, and logout functionality.

--- application/app.js
+++ application/app.js
@@ -7,54 +7,52 @@ const logger = require("morgan");
 const handlebars = require("express-handlebars");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const sessions = require('express-session');
+const mysqlSession = require('express-mysql-session')(sessions);
 
 const app = express();
 
-
-// const port = 3000;
-
-// app.listen(port,() => {   //new
-//     console.log(`Listening on localhost:${port}`);
-// })
-
-
-const db = require('./conf/database');
-const {log} = require("debug");
-// db.query("SELECT * FROM users", function(err, results, fields){
-//     if(err){
-//         console.log(err);
-//
-//     }else{
-//         results.forEach(row => console.log(row));
-//     }
-//     db.end();
-// });
-
 app.engine(
-  "hbs",
-  handlebars({
-    layoutsDir: path.join(__dirname, "views/layouts"), //where to look for layouts
-    partialsDir: path.join(__dirname, "views/partials"), // where to look for partials
-    extname: ".hbs", //expected file extension for handlebars files
-    defaultLayout: "layout", //default layout for app, general template for all pages in app
-    helpers: {}, //adding new helpers to handlebars for extra functionality
-  })
+    "hbs",
+    handlebars({
+        layoutsDir: path.join(__dirname, "views/layouts"), //where to look for layouts
+        partialsDir: path.join(__dirname, "views/partials"), // where to look for partials
+        extname: ".hbs", //expected file extension for handlebars files
+        defaultLayout: "layout", //default layout for app, general template for all pages in app
+        helpers: {}, //adding new helpers to handlebars for extra functionality
+    })
 );
+
+const mysqlSessionStore = new mysqlSession(
+    {/* Default Options*/
+    },
+    require('./conf/database')
+);
+app.use(sessions({
+    key: "csid",
+    secret: "csc317secret",
+    store: mysqlSessionStore,
+    resave: false,
+    saveUninitialized: false
+}));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
+    console.log(req.session);
+    if(req.session.username){
+        res.locals.logged = true;
+    }
     next();
 })
 
@@ -63,25 +61,25 @@ app.use("/users", usersRouter); // route middleware from ./routes/users.js
 
 
 /**
- * Catch all route, if we get to here then the 
+ * Catch all route, if we get to here then the
  * resource requested could not be found.
  */
-app.use((req,res,next) => {
-  next(createError(404, `The route ${req.method} : ${req.url} does not exist.`));
+app.use((req, res, next) => {
+    next(createError(404, `The route ${req.method} : ${req.url} does not exist.`));
 })
-  
+
 
 /**
  * Error Handler, used to render the error html file
  * with relevant error information.
  */
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = err;
-  console.log(err);
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    res.locals.message = err.message;
+    res.locals.error = err;
+    console.log(err);
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
--- application/routes/users.js
+++ application/routes/users.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../conf/database');
+const express = require('express');
+const router = express.Router();
+const db = require('../conf/database');
 const {errorPrint, successPrint} = require('../helpers/debug/debugprinters');
 const UserError = require("../helpers/error/UserError");
+const bcrypt = require('bcrypt');
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -28,18 +30,22 @@ router.post('/register', (req, res, next) => {
                     200
                 );
             }
-        }).then(([results, fields]) => {
-        if (results && results.length === 0) {
+        })
+        .then(([results, fields]) => {
+            if (results && results.length === 0) {
+                return bcrypt.hash(password, 15);
+            } else {
+                throw new UserError(
+                    "Registration Failed: Email already exists",
+                    "/register",
+                    200
+                );
+            }
+        })
+        .then((hashedPassword) => {
             let baseSQL = "INSERT INTO users(username, email, password, created) VALUES (?,?,?,now());"
-            return db.execute(baseSQL, [username, email, password]);
-        } else {
-            throw new UserError(
-                "Registration Failed: Email already exists",
-                "/register",
-                200
-            );
-        }
-    })
+            return db.execute(baseSQL, [username, email, hashedPassword]);
+        })
         .then(([results, fields]) => {
             if (results && results.affectedRows) {
                 successPrint("User was created!")
@@ -65,21 +71,34 @@ router.post('/register', (req, res, next) => {
 })
 
 /* Get form data from login form*/
-router.post('/login',(req,res,next) => {
+router.post('/login', (req, res, next) => {
     let username = req.body.username;
     let password = req.body.password;
 
     //TODO ServerSide Validation not done in video
 
-    let baseSQL = "SELECT username, password FROM users WHERE username=? AND password=?;"
-    db.execute(baseSQL,[username,password])
-        .then(([results,fields]) => {
-            if(results && results.length === 1){
+    let baseSQL = "SELECT id,username, password FROM users WHERE username=?;"
+    let userId;
+    db.execute(baseSQL, [username])
+        .then(([results, fields]) => {
+            if (results && results.length === 1) {
+                let hashedPassword = results[0].password;
+                userId = results[0].id;
+                return bcrypt.compare(password, hashedPassword);
+            } else {
+                throw new UserError("Invalid username and/or password!",
+                    "/login",
+                    200);
+            }
+        })
+        .then((passwordsMatched) => {
+            if (passwordsMatched) {
                 successPrint(`User ${username} is logged in`);
-                res.cookie("logged",username,{expires: new Date(Date.now()+ 900000), httpOnly: false});
+                req.session.username = username;
+                req.session.userId = userId;
                 res.locals.logged = true;
-                res.render('index');
-            }else{
+                res.redirect("/");
+            } else {
                 throw new UserError("Invalid username and/or password!",
                     "/login",
                     200);
@@ -87,14 +106,29 @@ router.post('/login',(req,res,next) => {
         })
         .catch((err) => {
             errorPrint("User login failed");
-            if(err instanceof UserError){
+            if (err instanceof UserError) {
                 errorPrint(err.getMessage());
                 res.status(err.getStatus());
                 res.redirect('/login');
-            }else{
+            } else {
                 next(err);
             }
         })
 })
+
+/* Logout*/
+router.post('/logout',(req,res,next) => {
+    req.session.destroy((err) => {
+        if(err){
+            errorPrint("Session could not be destroyed.");
+            next(err);
+        }else{
+            successPrint("Session was destroyed.");
+            res.clearCookie('csid');
+            res.json({status:"OK", message:"User is logged out" });
+        }
+    })
+});
+
 
 module.exports = router;
