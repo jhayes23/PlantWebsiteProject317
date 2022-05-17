2a36ab60033a1242e2c33198be7e6b3416d553ad
josh <rainiser@icloud.com>
2022-05-17 00:07:09 -0700

    worked on more css and fixed navbar. Final css touching up left

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,7 +6,7 @@ var db = require("../conf/database");
 
 /* GET home page. */
 router.get('/', getRecentPosts, function (req, res, next) {
-    res.render('index', {title: 'CSC 317 App', name: "Joshua Hayes"});
+    res.render('index', {title: 'CSC 317 App', username: `${req.session.username}`, name: "Joshua Hayes"});
 });
 
 /* GET login page. */
@@ -19,11 +19,11 @@ router.get('/register', (req, res, next) => {
     res.render('register', {title: 'Register'});
 });
 router.get('/post/:id(\\d+)',getPostById,getCommentsByPostId, (req, res, next) => {
-    res.render('imagepost', {title: `Post ${req.params.id}`});
+    res.render('imagepost', {title: `Post ${req.params.id}`, username: `${req.session.username}`});
 });
 router.use('/post', isLoggedIn);
 router.get('/post', (req, res, next) => {
-    res.render('postimage', {title: 'Post An Image'})
+    res.render('postimage', {title: 'Post An Image',username: `${req.session.username}`})
 });
 
 
